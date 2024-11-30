c11db5c60a429ad6b46e401dc2e2dbedc68dc93a
Ryan LeRoy <leroy.ryan09@gmail.com>
2024-11-30 09:27:08 -0800

    Created a school cookie

--- src/routes/[school]/+page.server.ts
+++ src/routes/[school]/+page.server.ts
@@ -2,13 +2,17 @@ import type { PageServerLoad } from './$types';
 import { prisma } from '$lib/server/prisma';
 import { redirect } from '@sveltejs/kit';
 
-export const load : PageServerLoad = async ({ params }) => {
+export const load : PageServerLoad = async ({ cookies, params }) => {
     const schoolData = await prisma.school.findFirst({ where: { webAddr: params.school }});
 
     // school addr dosent exist
     if (schoolData == undefined) {
         redirect(302, "/");
     }
+
+    cookies.set("school", schoolData.id, {
+		path: ".",
+    });
 
     return { schoolData };
 };

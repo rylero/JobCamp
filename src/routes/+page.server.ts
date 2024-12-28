976fa6a21731167c68fc4e178352fe0999b7506b
Ryan LeRoy <leroy.ryan09@gmail.com>
2024-12-27 17:59:26 -0800

    Required Name

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,6 @@
 import { PageType, userAccountSetupFlow } from '$lib/server/authFlow';
 import { redirect } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import { prisma } from '$lib/server/prisma';
 
 export const load: PageServerLoad = async (event) => {
     redirect(302, "/lghs");

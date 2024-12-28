ac600b5974bcbab584c05f39d6cbdcad54effcaa
Ryan LeRoy <leroy.ryan09@gmail.com>
2024-12-27 18:43:28 -0800

    Padding On Title

--- src/routes/[school]/+page.svelte
+++ src/routes/[school]/+page.svelte
@@ -24,6 +24,6 @@
 </nav>
 
 <div class="w-screen h-screen bg-cover bg-center flex flex-col justify-center items-center" style="background-image: url({JobCampBanner});">
-    <h1 class="text-8xl md:text-9xl text-black">JobCamp</h1>
+    <h1 class="text-8xl md:text-9xl text-black px-4">JobCamp</h1>
     <Button class="text-2xl mt-8 py-6 shadow-2xl" href={loggedIn ? "/dashboard" : "/signup"}>{ loggedIn ? "Dashboard" : "Sign Up" }</Button>
 </div>

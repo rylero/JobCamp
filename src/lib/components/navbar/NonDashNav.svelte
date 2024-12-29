6a4c07f2e744013555690d13b5f696bc3dfdf409
Ryan LeRoy <leroy.ryan09@gmail.com>
2024-12-28 19:53:24 -0800

    Links

--- src/lib/components/navbar/NonDashNav.svelte
+++ src/lib/components/navbar/NonDashNav.svelte
@@ -7,13 +7,13 @@
 <nav class="w-screen h-20 fixed top-0 left-0 bg-gray-800 flex flex-row justify-between items-center text-white">
     <a href="/{schoolData.webAddr}" class="ml-4 text-2xl">JobCamp | {schoolData.name}</a>
     <div class="flex flex-row gap-4 mr-4">
-        <Button href="/host-tips" variant="secondary" class="bg-white text-xl">Host Tips</Button>
-        <Button href="/faq" variant="secondary" class="bg-white text-xl">FAQ</Button>
+        <Button href="/host-tips" variant="link" class="text-white text-xl">Host Tips</Button>
+        <Button href="/faq" variant="link" class="text-white text-xl">FAQ</Button>
         {#if !loggedIn}
-            <Button href="/signup" variant="secondary" class="bg-white text-xl">Sign Up</Button>
-            <Button href="/login" variant="secondary" class="bg-white text-xl">Login</Button>
+            <Button href="/signup" variant="link" class="text-white text-xl">Sign Up</Button>
+            <Button href="/login" variant="link" class="text-white text-xl">Login</Button>
         {:else}
-            <Button href="/dashboard" variant="secondary" class="bg-white text-xl">Dashboard</Button>
+            <Button href="/dashboard" variant="link" class="text-white text-xl">Dashboard</Button>
         {/if}
     </div>
 </nav>

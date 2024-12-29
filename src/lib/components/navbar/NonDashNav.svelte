8f49d4eb9012ed84df51ace1cfbe992ac0ce8bad
Ryan LeRoy <leroy.ryan09@gmail.com>
2024-12-28 19:35:46 -0800

    Clickable JobCamp Link

--- src/lib/components/navbar/NonDashNav.svelte
+++ src/lib/components/navbar/NonDashNav.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <nav class="w-screen h-20 fixed top-0 left-0 bg-gray-800 flex flex-row justify-between items-center text-white">
-    <h1 class="ml-4 text-2xl">JobCamp | {schoolData.name}</h1>
+    <a href="/{schoolData.webAddr}" class="ml-4 text-2xl">JobCamp | {schoolData.name}</a>
     <div class="flex flex-row gap-4 mr-4">
         <Button href="/faq" variant="secondary" class="bg-white text-xl">FAQ</Button>
         {#if !loggedIn}

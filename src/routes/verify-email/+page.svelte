81edc6790a5c488bc294a77968486b8f80906d5a
Ryan LeRoy <leroy.ryan09@gmail.com>
2024-12-31 19:06:48 -0800

    Msg Verify Email Show on screen

--- src/routes/verify-email/+page.svelte
+++ src/routes/verify-email/+page.svelte
@@ -2,15 +2,15 @@
     import Button from "$lib/components/ui/button/button.svelte";
     import Navbar from "$lib/components/navbar/Navbar.svelte";
 
-    var { data } = $props();
+    var { data, form } = $props();
 
-    var form: HTMLFormElement;
+    var formEl: HTMLFormElement;
 </script>
 
 <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col justify-between items-center gap-4 py-10 px-10 sm:border-2 sm:rounded-lg sm:shadow-2xl">
     <h1 class="text-xl text-center">Check your inbox for an email verification link</h1>
-    <p class="text-red">{data.msg}</p>
-    <form method="POST" action="?/resend" bind:this={form}>
-        <Button onclick={() => form.submit()} class="text-xl mt-2 w-30 h-10 rounded bg-blue-500 text-white hover:bg-blue-600">Resend</Button>
+    {#if form && form.msg}<p class="text-red">{form.msg}</p>{/if}
+    <form method="POST" action="?/resend" bind:this={formEl}>
+        <Button onclick={() => formEl.submit()} class="text-xl mt-2 w-30 h-10 rounded bg-blue-500 text-white hover:bg-blue-600">Resend</Button>
     </form>
 </div> 

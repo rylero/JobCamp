f2705de5346313722f8a1df905c0198f54550f62
Ryan LeRoy <leroy.ryan09@gmail.com>
2024-12-31 19:14:03 -0800

    console logs

--- src/routes/verify-email/+page.server.ts
+++ src/routes/verify-email/+page.server.ts
@@ -23,21 +23,27 @@ export const actions: Actions = {
             return { msg: "Incorrect Link. Please contact support at admin@jobcamp.org."};
         }
 
+        console.log("user id");
         const userId = form.get("uid")?.toString();
+        console.log("userID: " + userId)
         if (!userId) { redirect(302, "/signup"); }
         
+        console.log("get user");
         const correctCode = await prisma.emailVerificationCodes.findFirst({
             where: { user_id: userId }
         });
 
+        console.log("link");
         if (!correctCode || correctCode.code != code) {
             return { msg: "Incorrect Link. Please Resend and Try again."}
         }
 
+        console.log("expire");
         if (correctCode.expires_at < new Date()) {
             return { msg: "Expired Link. Please Resend and Try again."}
         }
 
+        console.log("user");
         await prisma.user.update({
             where: { id: userId },
             data: {
@@ -45,6 +51,7 @@ export const actions: Actions = {
             }
         });
         
+        console.log("verify");
         await prisma.emailVerificationCodes.delete({
             where: { user_id: userId }
         });

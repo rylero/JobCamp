1b2ce340b6e43129eebcc6ab23e0c03d69f9b964
Ryan LeRoy <leroy.ryan09@gmail.com>
2025-01-02 12:16:35 -0800

    Font size

--- src/routes/[school]/+page.svelte
+++ src/routes/[school]/+page.svelte
@@ -12,6 +12,6 @@
 <Navbar isHost={isHost} loggedIn={loggedIn} />
 
 <div class="w-screen h-screen bg-cover bg-center flex flex-col justify-center items-center" style="background-image: url({JobCampBanner});">
-    <h1 class="text-8xl md:text-9xl text-black px-4">JobCamp</h1>
+    <h1 class="text-7xl md:text-9xl text-black px-4">JobCamp</h1>
     <Button class="text-2xl mt-8 py-6 shadow-2xl" href={loggedIn ? "/dashboard" : "/signup"}>{ loggedIn ? "Dashboard" : "Sign Up" }</Button>
 </div>

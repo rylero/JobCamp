7c7a83d667ea91921531a947a7f4ef05073c0be3
Ryan LeRoy <leroy.ryan09@gmail.com>
2025-01-02 17:15:46 -0800

    Default slots

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,18 +1,24 @@
 <script lang="ts">
     import Button from "$lib/components/ui/button/button.svelte";
-    import type { X } from "lucide-svelte";
-    import { onMount } from "svelte";
-    import type { FormEventHandler } from "svelte/elements";
 
     var textarea: HTMLTextAreaElement;
+
+
+    function addMacro(macro: string) {
+        if (textarea) {
+            let pre = textarea.value.substring(0, textarea.selectionEnd);
+            let post = textarea.value.substring(textarea.selectionEnd);
+            textarea.value = pre + macro + post;
+        }
+    }
 </script>
 
 
 <div class="border rounded border-black w-[640px] m-10 flex flex-col">
-    <textarea rows="20" bind:this={textarea}></textarea>
-    <h1>Company</h1>
-    <div class="flex flex-row">
-        <Button variant="outline">Company Name</Button>
-        <Button>Host Name</Button>
+    <textarea rows="20" class="p-2" bind:this={textarea}></textarea>
+    <h1 class="text-xl border-t border-t-black pl-3 py-2">Company</h1>
+    <div class="flex flex-row gap-1 pl-3 pb-3">
+        <Button class="p-2 h-8 text-sm" onclick={() => addMacro("${company.name}")}>Company Name</Button>
+        <Button class="p-2 h-8 text-sm" onclick={() => addMacro("${host.name}")}>Host Name</Button>
     </div>
 </div>

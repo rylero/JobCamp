76b14b20d2cbca59dad63d8b9f9dba7ec689ecd9
Ryan LeRoy <leroy.ryan09@gmail.com>
2025-01-03 12:31:13 -0800

    Revert verification code

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -1,24 +1,53 @@
 <script lang="ts">
     import Button from "$lib/components/ui/button/button.svelte";
+    import Label from "$lib/components/ui/label/label.svelte";
 
-    var textarea: HTMLTextAreaElement;
-
-
-    function addMacro(macro: string) {
-        if (textarea) {
-            let pre = textarea.value.substring(0, textarea.selectionEnd);
-            let post = textarea.value.substring(textarea.selectionEnd);
-            textarea.value = pre + macro + post;
+    let query = $state({
+        hosts: {
+            active: false,
+            emailVerified: { value: false, active: false },
+            positionCount: { value: -1, active: false },
         }
-    }
+    });
+
+    let queryString = $derived(JSON.stringify(query))
 </script>
 
-
-<div class="border rounded border-black w-[640px] m-10 flex flex-col">
-    <textarea rows="20" class="p-2" bind:this={textarea}></textarea>
-    <h1 class="text-xl border-t border-t-black pl-3 py-2">Company</h1>
-    <div class="flex flex-row gap-1 pl-3 pb-3">
-        <Button class="p-2 h-8 text-sm" onclick={() => addMacro("${company.name}")}>Company Name</Button>
-        <Button class="p-2 h-8 text-sm" onclick={() => addMacro("${host.name}")}>Host Name</Button>
+<div class="flex">
+    <div class="p-4 border-r-2 border-slate-900 min-h-screen min-w-[28rem]">
+        <Button class="text-lg" onclick={() => query.hosts.active = !query.hosts.active}>Hosts</Button>
+        {#if query.hosts.active}
+        <div class="pl-8 pt-2 flex flex-col gap-2">
+            <Label class="flex justify-between items-center text-lg w-full">
+                <span class="flex gap-3 items-center"><input bind:checked={query.hosts.name.active} class="w-4 h-4" type="checkbox" />Name</span>
+                <input bind:value={query.hosts.name.value} class="border border-slate-950 rounded px-2 w-48"/>
+            </Label>
+            <Label class="flex justify-between items-center text-lg w-full">
+                <span class="flex gap-3 items-center"><input bind:checked={query.hosts.emailVerified.active} class="w-4 h-4" type="checkbox" />Email Verified</span>
+                <select bind:value={query.hosts.emailVerified.value} class="border border-slate-950 rounded px-2">
+                    <option value={true}>True</option>
+                    <option value={false}>False</option>
+                </select>
+            </Label>
+            <Label class="text-lg">Company</Label>
+            <div class="pl-8 flex flex-col gap-2">
+                <Label class="flex justify-between items-center text-lg w-full">
+                    <span class="flex gap-3 items-center"><input bind:checked={query.hosts.company.name.active} class="w-4 h-4" type="checkbox" />Name</span>
+                    <input bind:value={query.hosts.company.name.value} class="border border-slate-950 rounded px-2 w-48" />
+                </Label>
+                <Label class="flex justify-between items-center text-lg w-full">
+                    <span class="flex gap-3 items-center"><input bind:checked={query.hosts.company.description.active} class="w-4 h-4" type="checkbox" />Description</span>
+                    <input bind:value={query.hosts.company.description.value} class="border border-slate-950 rounded px-2 w-48" />
+                </Label>
+                <Label class="flex justify-between items-center text-lg w-full">
+                    <span class="flex gap-3 items-center"><input bind:checked={query.hosts.company.url.active} class="w-4 h-4" type="checkbox" />URL</span>
+                    <input bind:value={query.hosts.company.url.value} class="border border-slate-950 rounded px-2 w-48" />
+                </Label>
+            </div>
+        </div>
+        {/if}
+    </div>
+    <div class="p-4">
+        {queryString}
     </div>
 </div>

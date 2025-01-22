4f23b245f7fabfede20a62681d578fbea7183339
Ryan LeRoy <leroy.ryan09@gmail.com>
2025-01-21 19:17:16 -0800

    Fix Border

--- src/routes/dashboard/student/+page.svelte
+++ src/routes/dashboard/student/+page.svelte
@@ -7,10 +7,10 @@
 
 <Navbar loggedIn={true} isHost={false} />
 
-<div class="flex w-full h-screen pt-20">
-    <div class="flex flex-col min-w-72 gap-2 h-full justify-start items-start p-4 border-r-2 border-r-slate-950">
+<div class="flex w-full min-h-screen pt-20">
+    <div class="flex flex-col min-w-72 gap-2 justify-start items-start p-4">
     </div>
-    <div class="flex flex-col w-full h-full">
+    <div class="flex flex-col w-full border-l-2 border-l-slate-950">
         {#each dates as info}
             <div class="m-2 p-4 rounded-md shadow-xl border-2">
                 <h1 class="text-xl mb-2">{ info.date } - { info.title }</h1>

3ee438b4d86ae46af2ad2ecbebd46f513bd637d5
Ryan LeRoy <leroy.ryan09@gmail.com>
2025-01-25 14:24:44 -0800

    Picks Screen

--- src/routes/dashboard/student/pick/+page.server.ts
+++ src/routes/dashboard/student/pick/+page.server.ts
@@ -3,14 +3,11 @@ import { redirect } from '@sveltejs/kit';
 import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ params, locals }) => {
-    const loggedIn = locals.user != null;
-    
-    let isHost = false;
-    if (locals.user) {
-        isHost = locals.user.host != null;
+    if (!locals.user) {
+        redirect(302, "/login");
     }
 
-    const schoolWebAddr = params.school;
+    const schoolWebAddr = "lghs";
 
     const school = await prisma.school.findFirst({ where: { webAddr: schoolWebAddr } });
 
@@ -37,5 +34,85 @@ export const load: PageServerLoad = async ({ params, locals }) => {
         }
     });
 
-    return { positionData, isHost, loggedIn };
+    const student = await prisma.student.findFirst({where: {userId: locals.user.id}});
+    const studentId = student?.id;
+    if (!studentId) {
+        redirect(302, "/login");
+    }
+
+    const positionsOnStudents = await prisma.positionsOnStudents.findMany({ where: {
+        studentId: studentId
+    }})
+
+    positionData.map((val: any) => {
+        val.selected = false;
+        positionsOnStudents.forEach(a => {
+            if (a.positionId == val.id) {
+                val.selected = true;
+            }
+        })
+        return val;
+    })
+
+    const posData: any = positionData;
+
+    return { positionData: posData };
+}
+
+
+export const actions: Actions = {
+    togglePosition: async ({ request, locals, cookies }) => {
+        const data = await request.formData();
+
+        const posId = data.get("id")?.toString();
+        if (!posId) {
+            redirect(302, "/about");
+        }
+
+        const id = locals.user?.id;
+        if (!id) {
+            redirect(302, "/login");
+        }
+
+        const student = await prisma.student.findFirst({where: {userId: id}});
+        const studentId = student?.id;
+        if (!studentId) {
+            redirect(302, "/login");
+        }
+
+        const positionOnStudent = await prisma.positionsOnStudents.deleteMany({
+            where: { studentId: studentId, positionId: posId}
+        })
+
+        if (positionOnStudent.count == 0) { // no record exsited, we should add one as this means the checkbox was just checked
+            const highestRank = await prisma.positionsOnStudents.findFirst({
+                where: { studentId: studentId },
+                orderBy: {
+                    rank: "desc"
+                },
+            });
+
+            let max = 0;
+            if (highestRank) {
+                max = highestRank.rank+1;
+            }
+
+
+            const res = await prisma.positionsOnStudents.create({
+                data: {
+                    student: {
+                        connect: {
+                            id: studentId,
+                        }
+                    },
+                    position: {
+                        connect: {
+                            id: posId,
+                        }
+                    },
+                    rank: max
+                }
+            });
+        }
+    }
 }
--- src/routes/dashboard/student/pick/+page.svelte
+++ src/routes/dashboard/student/pick/+page.svelte
@@ -2,6 +2,8 @@
     import Navbar from '$lib/components/navbar/Navbar.svelte';
     import Button from '$lib/components/ui/button/button.svelte';
     import * as Accordion from "$lib/components/ui/accordion/index.js";
+    import { Input } from '$lib/components/ui/input';
+    import { enhance } from '$app/forms';
 
     let { data } = $props();
 
@@ -12,7 +14,7 @@
     let selectedTerm = $state("");
 
     let filteredPositions = $derived((() => {
-        return data.positionData.filter((value) => {
+        return data.positionData.filter((value: any) => {
             if (selected == "career") {
                 return value.career == selectedTerm;
             } else {
@@ -23,7 +25,7 @@
 
     let terms = $derived((() => {
         let possible: any = {};
-        data.positionData.forEach((value) => {
+        data.positionData.forEach((value: any) => {
             if (selected == "career") {
                 possible[value.career] = 1;
             } else {
@@ -40,6 +42,23 @@
             return;
         }
         selectedTerm = term;
+    }
+
+    const togglePosition = async (posID: string) => {
+        const fdata = new FormData();
+        fdata.append("id", posID);
+
+        await fetch("?/togglePosition", {
+            method: "post",
+            body: fdata
+        });
+
+        data.positionData.map((val: any) => {
+            if (val.id == posID) {
+                val.selected = !val.selected;
+            }
+            return val;
+        })
     }
 </script>
 
@@ -64,6 +83,8 @@
                             <span>{position.host?.company?.companyName} - {position.title}</span>
                         </Accordion.Trigger>
                         <Accordion.Content class="px-5">
+                            <label class="flex gap-2 text-lg my-3 items-center"><input type="checkbox" name="selected" bind:checked={position.selected} onchange={() => togglePosition(position.id)} /> Add to My Favorite Jobs</label>
+
                             <p class="mt-1">Career: { position.career }</p><br>
                             <p class="mt-1">Description: { position.host?.company?.companyDescription}</p>
                             <p class="mt-1">URL: {position.host?.company?.companyUrl}</p>
@@ -90,6 +111,8 @@
                             <p class="">Arrival: { position.arrival }</p>
                             <p class="">Start: { position.start }</p>
                             <p class="">End: { position.end }</p>
+
+                            <Input type="checkbox" />
                         </Accordion.Content>
                       </Accordion.Item>
                     {/each}
@@ -103,12 +126,14 @@
             <h1 class="text-xl text-center mt-5">Please select a carrer or company to view positions.</h1>
         {/if}
         <Accordion.Root class="w-full px-10 my-5">
-            {#each filteredPositions as position}
+            {#each filteredPositions as position, index}
               <Accordion.Item value={position.id} class="my-2">
                 <Accordion.Trigger class="text-xl bg-slate-100 hover:bg-slate-200 rounded-t-sm px-5">
                     <span>{position.host?.company?.companyName} - {position.title}</span>
                 </Accordion.Trigger>
                 <Accordion.Content class="px-5">
+                    <label class="flex gap-2 text-lg my-3 items-center"><input type="checkbox" name="selected" bind:checked={position.selected} onchange={() => togglePosition(position.id)} /> Add to My Favorite Jobs</label>
+
                     <p class="mt-1">Career: { position.career }</p><br>
                     <p class="mt-1">Description: { position.host?.company?.companyDescription}</p>
                     <p class="mt-1">URL: {position.host?.company?.companyUrl}</p>

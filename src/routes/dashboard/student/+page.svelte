0b734cad14302ebd455ec13c7c59c73b80e4325f
Ryan LeRoy <leroy.ryan09@gmail.com>
2025-01-31 21:20:44 -0800

    More Space?

--- src/routes/dashboard/student/+page.svelte
+++ src/routes/dashboard/student/+page.svelte
@@ -70,18 +70,18 @@
 <Navbar loggedIn={true} isHost={false} />
 
 <div class="flex flex-col md:flex-row w-full min-h-screen pt-20">
-    <div class={"w-full flex flex-col gap-2 justify-start items-start md:m-4" + leftWidth}>
-        <h1 class="text-2xl text-center w-full">My Favorite Jobs</h1>
+    <div class={"w-full flex flex-col gap-2 justify-start items-center md:m-4" + leftWidth}>
+        <h1 class="text-2xl text-center w-full mt-4 md:mt-0">My Favorite Jobs</h1>
         {#if positions.posList.length != 0}
-            <Accordion.Root type="multiple"  class="mx-4 mt-2 w-[calc(100%-64px)]">
+            <Accordion.Root type="multiple"  class="mt-2 w-[calc(100%-64px)]">
                 {#each positions.posList as position, i}
                     <Accordion.Item value={position.id} class="my-2 relative">
-                    <Accordion.Trigger class={"relative text-xl text-left bg-slate-100 hover:bg-slate-200 rounded-t-sm px-5" + ((i == positions.posList.length-1 || i == 0) ? " min-h-[60px]" : " min-h-[90px]")}>
+                    <Accordion.Trigger class={"relative text-md md:text-lg text-left bg-slate-100 hover:bg-slate-200 rounded-t-sm px-5 pl-9" + ((i == positions.posList.length-1 || i == 0) ? " min-h-[60px]" : " min-h-[110px]")}>
                         <span class="pl-12 pr-32 text-wrap">{position.host?.company?.companyName} - {position.title}</span>
                     </Accordion.Trigger>
-                    {#if i != positions.posList.length-1}<ArrowBigDown class={"absolute left-5 hover:cursor-pointer" + ((i == 0) ? " top-3" : " top-12")} size={32} onclick={() => moveUp(position.id)} />{/if}
-                        {#if i != 0}<ArrowBigUp class="absolute top-3 left-5 hover:cursor-pointer" size={32} onclick={() => moveDown(position.id)} />{/if}
-                        <Trash2Icon class={"absolute right-[60px] hover:cursor-pointer" + ((i == positions.posList.length-1 || i == 0) ? " top-3" : " top-7")} onclick={() => deletePosition(position.id)} size={32} />
+                    {#if i != positions.posList.length-1}<ArrowBigDown class={"absolute left-3 hover:cursor-pointer" + ((i == 0) ? " top-2" : " top-12")} size={48} onclick={() => moveUp(position.id)} />{/if}
+                    {#if i != 0}<ArrowBigUp class="absolute top-3 left-3 hover:cursor-pointer" size={48} onclick={() => moveDown(position.id)} />{/if}
+                    <Trash2Icon class={"absolute right-[60px] hover:cursor-pointer" + ((i == positions.posList.length-1 || i == 0) ? " top-4" : " top-10")} onclick={() => deletePosition(position.id)} size={32} />
                     <Accordion.Content class="px-5">
                         <p class="mt-1">Career: { position.career }</p><br>
                         <p class="mt-1">Description: { position.host?.company?.companyDescription}</p>
@@ -120,7 +120,7 @@
     <div class="flex flex-col w-full md:border-l-2 md:border-l-slate-950">
         <h1 class="text-2xl px-4 py-4 text-center w-full">Important Dates</h1>
         {#each dates as info}
-            <div class="m-2 p-4 rounded-md shadow-xl border-2">
+            <div class="m-2 mx-4 p-4 rounded-md shadow-xl border-2">
                 <h1 class="text-xl mb-2">{ info.date } - { info.title }</h1>
                 <p>{ info.description }</p>
             </div>

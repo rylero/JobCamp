df03fbf5a7b16db44a9e44742eabd84aeb0d1983
Ryan LeRoy <leroy.ryan09@gmail.com>
2025-01-31 19:53:21 -0800

    Pick Favorites Order

--- src/routes/dashboard/student/+page.server.ts
+++ src/routes/dashboard/student/+page.server.ts
@@ -1,5 +1,5 @@
 import { redirect } from "@sveltejs/kit";
-import type { PageServerLoad } from "./$types";
+import type { Actions, PageServerLoad } from "./$types";
 import { prisma } from "$lib/server/prisma";
 
 export const load: PageServerLoad = async (event) => {
@@ -34,7 +34,126 @@ export const load: PageServerLoad = async (event) => {
 
     let positions = positionsOnStudents.map(val => val.position);
 
-    console.log(positions);
-
     return { positions: positions };
 };
+
+
+export const actions: Actions = {
+    deletePosition: async({ request, locals, cookies }) => {
+        const data = await request.formData();
+
+        const posId = data.get("id")?.toString();
+        if (!posId) {
+            redirect(302, "/about");
+        }
+
+        const id = locals.user?.id;
+        if (!id) {
+            redirect(302, "/login");
+        }
+
+        const student = await prisma.student.findFirst({where: {userId: id}});
+        const studentId = student?.id;
+        if (!studentId) {
+            redirect(302, "/login");
+        }
+
+        let positionsOnStudents = await prisma.positionsOnStudents.findMany({
+            where: {studentId: student.id},
+            orderBy: { rank: "asc" },
+            include: { position: {
+                include: {
+                    host: {
+                        include: {
+                            company: true
+                        }
+                    }
+                }
+            } }
+        });
+
+        await prisma.positionsOnStudents.deleteMany({
+            where: { studentId: studentId}
+        })
+
+        let newPositionsOnStudents = positionsOnStudents.filter(val => val.positionId != posId);
+
+        let positions = newPositionsOnStudents.map((val) => {
+            return { 
+                rank: val.rank, 
+                studentId: val.studentId,
+                positionId: val.positionId,
+            };
+        })
+
+        await prisma.positionsOnStudents.createMany({
+            data: positions
+        });
+    },
+    move: async({request, locals, cookies}) => {
+        const data = await request.formData();
+
+        const posId = data.get("id")?.toString();
+        if (!posId) {
+            redirect(302, "/about");
+        }
+
+        const direction = data.get("dir")?.toString();
+        if (!direction) {
+            redirect(302, "/about");
+        }
+
+        const id = locals.user?.id;
+        if (!id) {
+            redirect(302, "/login");
+        }
+
+        const student = await prisma.student.findFirst({where: {userId: id}});
+        const studentId = student?.id;
+        if (!studentId) {
+            redirect(302, "/login");
+        }
+
+        let positionsOnStudents = await prisma.positionsOnStudents.findMany({
+            where: {studentId: student.id},
+            orderBy: { rank: "asc" },
+            include: { position: {
+                include: {
+                    host: {
+                        include: {
+                            company: true
+                        }
+                    }
+                }
+            } }
+        });
+
+        await prisma.positionsOnStudents.deleteMany({
+            where: { studentId: studentId}
+        })
+
+        let posRankIndex = 0;
+        for (let i = 0; i < positionsOnStudents.length; i++) {
+            if (positionsOnStudents[i].positionId == posId) {
+                posRankIndex = i;
+            }
+        }
+
+        var dir = direction == "down" ? -1 : 1;
+        var temp = positionsOnStudents[posRankIndex - dir];
+        positionsOnStudents[posRankIndex - dir] = positionsOnStudents[posRankIndex];
+        positionsOnStudents[posRankIndex] = temp;
+
+        let positions = positionsOnStudents.map((val, i) => {
+            return { 
+                rank: i, 
+                studentId: val.studentId,
+                positionId: val.positionId,
+            };
+        })
+
+        await prisma.positionsOnStudents.createMany({
+            data: positions
+        });
+    }
+}
--- src/routes/dashboard/student/+page.svelte
+++ src/routes/dashboard/student/+page.svelte
@@ -1,24 +1,86 @@
 <script lang="ts">
     import Navbar from "$lib/components/navbar/Navbar.svelte";
     import * as Accordion from "$lib/components/ui/accordion/index.js";
+    import { ArrowBigDown, ArrowBigUp, Trash2Icon } from "lucide-svelte";
     import { dates } from "./important-dates.json";
 
     let { data } = $props();
 
-    let leftWidth = $derived(data.positions.length == 0 ? " w-72" : " w-full");
+    let positions = $state({ posList: data.positions });
+    let leftWidth = $derived(positions.posList.length == 0 ? " w-72" : " min-w-[32rem] w-full");
+
+
+    const deletePosition = async (posID: string) => {
+        positions.posList = positions.posList.filter((val) => val.id != posID);
+
+        const fdata = new FormData();
+        fdata.append("id", posID);
+
+        await fetch("?/deletePosition", {
+            method: "post",
+            body: fdata
+        });
+    }
+
+    const moveUp = async (posID: string) => {
+        let posRankIndex = 0;
+        for (let i = 0; i < positions.posList.length; i++) {
+            if (positions.posList[i].id == posID) {
+                posRankIndex = i;
+            }
+        }
+
+        var temp = positions.posList[posRankIndex + 1];
+        positions.posList[posRankIndex + 1] = positions.posList[posRankIndex];
+        positions.posList[posRankIndex] = temp;
+
+        const fdata = new FormData();
+        fdata.append("id", posID);
+        fdata.append("dir", "down");
+
+        await fetch("?/move", {
+            method: "post",
+            body: fdata
+        });
+    }
+
+    const moveDown = async (posID: string) => {
+        let posRankIndex = 0;
+        for (let i = 0; i < positions.posList.length; i++) {
+            if (positions.posList[i].id == posID) {
+                posRankIndex = i;
+            }
+        }
+
+        var temp = positions.posList[posRankIndex - 1];
+        positions.posList[posRankIndex - 1] = positions.posList[posRankIndex];
+        positions.posList[posRankIndex] = temp;
+
+        const fdata = new FormData();
+        fdata.append("id", posID);
+        fdata.append("dir", "up");
+
+        await fetch("?/move", {
+            method: "post",
+            body: fdata
+        });
+    }
 </script>
 
 <Navbar loggedIn={true} isHost={false} />
 
-<div class="flex w-full min-h-screen pt-20">
+<div class="flex flex-col md:flex-row w-full min-h-screen pt-20">
     <div class={"flex flex-col gap-2 justify-start items-start p-4" + leftWidth}>
         <h1 class="text-2xl pb-4">My Favorite Jobs</h1>
-        {#if data.positions.length != 0}
+        {#if positions.posList.length != 0}
         <Accordion.Root class="w-full">
-            {#each data.positions as position}
-                <Accordion.Item value={position.id} class="my-2">
-                <Accordion.Trigger class="text-xl text-left bg-slate-100 hover:bg-slate-200 rounded-t-sm px-5">
-                    <span>{position.host?.company?.companyName} - {position.title}</span>
+            {#each positions.posList as position, i}
+                <Accordion.Item value={position.id} class="my-2 relative">
+                {#if i != positions.posList.length-1}<ArrowBigDown class={"absolute left-5 hover:cursor-pointer" + ((i == 0) ? " top-12" : " top-20")} size={32} onclick={() => moveUp(position.id)} />{/if}
+                {#if i != 0}<ArrowBigUp class="absolute top-12 left-5 hover:cursor-pointer" size={32} onclick={() => moveDown(position.id)} />{/if}
+                <Trash2Icon class="absolute left-[20px] top-3 hover:cursor-pointer" onclick={() => deletePosition(position.id)} size={32} />
+                <Accordion.Trigger class={"text-xl text-left bg-slate-100 hover:bg-slate-200 rounded-t-sm px-5" + ((i == positions.posList.length-1 || i == 0) ? " h-[90px]" : " h-[120px]")}>
+                    <span class="pl-12 pr-2">{position.host?.company?.companyName} - {position.title}</span>
                 </Accordion.Trigger>
                 <Accordion.Content class="px-5">
                     <p class="mt-1">Career: { position.career }</p><br>
@@ -55,7 +117,7 @@
             <h1 class="text-center">No favorite jobs selected.</h1>
         {/if}
     </div>
-    <div class="flex flex-col w-full border-l-2 border-l-slate-950">
+    <div class="flex flex-col w-full md:border-l-2 md:border-l-slate-950">
         <h1 class="text-2xl px-4 py-4">Important Dates</h1>
         {#each dates as info}
             <div class="m-2 p-4 rounded-md shadow-xl border-2">
